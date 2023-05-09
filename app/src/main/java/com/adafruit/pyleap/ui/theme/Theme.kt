b87b6a247609de63797582adc0342c1b1d57c8f8
Antonio <antonio@openroad.es>
2023-05-09 18:36:21 +0200

    Fix wrong background colors when using Dark Theme

--- app/src/main/java/com/adafruit/pyleap/ui/theme/Theme.kt
+++ app/src/main/java/com/adafruit/pyleap/ui/theme/Theme.kt
@@ -41,7 +41,7 @@ private val LightColorScheme = lightColorScheme(
 
 @Composable
 fun PyLeapTheme(
-        darkTheme: Boolean = isSystemInDarkTheme(),
+        darkTheme: Boolean = false, //isSystemInDarkTheme(),    // TODO: add support for dark theme
         // Dynamic color is available on Android 12+
         dynamicColor: Boolean = true,
         content: @Composable () -> Unit
